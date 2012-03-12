3e90a63dade0e9530a785a7f8f6a8b262701392e
Joseph Chapman <joe@creatifyjs.com>
2012-03-12 14:51:44 +0000

    Cleaned up white space in test

--- specs/CreatifyJS.customEvents/CreatifyJS.customEvents.js
+++ specs/CreatifyJS.customEvents/CreatifyJS.customEvents.js
@@ -2,64 +2,49 @@ describe("Custom events", function () {
 
 	describe("Calling on() with an event and a callback function", function () {
 		var observer = CreatifyJS.customEvents;
-		
 		beforeEach(function () {
-			observer.on('newEvent', function callback () {});	
+			observer.on('newEvent', function callback () {});
 		});
-
 		afterEach(function () {
-			observer.events = {};	
+			observer.events = {};
 		});
-		
 		it("Should add the callback function to the new event", function () {
 			expect(observer.events['newEvent']).toBeDefined();
 			expect(typeof observer.events['newEvent'][0]).toBe('function');
 			expect(observer.events['newEvent'][0].name).toBe('callback');
 		});
-
 	});
-
 	describe("Calling on() with the same event and multiple callback functions", function () {
 		var observer = CreatifyJS.customEvents;
-		
 		beforeEach(function () {
 			observer.on('newEvent', function callback1 () {});
-			observer.on('newEvent', function callback2() {});
-			observer.on('newEvent', function callback3 () {});	
+			observer.on('newEvent', function callback2 () {});
+			observer.on('newEvent', function callback3 () {});
 		});
-
 		afterEach(function () {
-			observer.events = {};	
+			observer.events = {};
 		});
-		
 		it("Should add each callback function to the event", function () {
 			expect(observer.events['newEvent'][0].name).toBe('callback1');
 			expect(observer.events['newEvent'][1].name).toBe('callback2');
 			expect(observer.events['newEvent'][2].name).toBe('callback3');
 		});
 	});
-
 	describe("Calling on() without a callback function", function () {
 		var observer = CreatifyJS.customEvents;
-		
 		beforeEach(function () {
-			observer.on('newEvent');	
+			observer.on('newEvent');
 		});
-		
 		afterEach(function () {
-			observer.events = {};	
+			observer.events = {};
 		});
-
 		it("Should not add the new event", function () {
 			expect(observer.events['newEvent']).not.toBeDefined();
 		});
-
 	});
-
 	describe("Calling off() with an event", function () {
 		var observer = CreatifyJS.customEvents,
 			callBack1 = function () {};
-
 		beforeEach(function () {
 			// Add multiple callbacks to the event
 			observer.events = {
@@ -67,23 +52,18 @@ describe("Custom events", function () {
 			};
 			observer.off('newEvent');
 		});
-
 		afterEach(function () {
-			observer.events = {};	
+			observer.events = {};
 		});
-		
 		it("Should remove the event and its callback function", function () {
 			expect(observer.events['newEvent'][0]).not.toBeDefined();
 			expect(observer.events['newEvent'].length).toBe(0);
 		});
 	});
-
 	describe("Calling off() with an event and a callback", function () {
 	 	var observer = CreatifyJS.customEvents;
-
 		describe("When only one callback is registered to the event", function () {
 			var callBack1 = function () {};
-
 			beforeEach(function () {
 				// Add multiple callbacks to the event
 				observer.events = {
@@ -91,23 +71,18 @@ describe("Custom events", function () {
 				};
 				observer.off('newEvent', callBack1);
 			});
-
 			afterEach(function () {
 				observer.events = {};
 			});
-
 			it("Should removes the callback function", function () {
 				expect(observer.events['newEvent'][0]).not.toBeDefined();
 			});
-
 		});
-
 		describe("When multiple callbacks are registered", function () {
 			var callBack1 = function () {},
 				callBack2 = function () {},
 				callBack3 = function () {},
 				callBack4 = function () {};
-
 			beforeEach(function () {
 				// Add multiple callbacks to the event
 				observer.events = {
@@ -116,23 +91,17 @@ describe("Custom events", function () {
 				// Remove one callback
 				observer.off('newEvent', callBack4);
 			});
-
 			afterEach(function () {
-				observer.events = {};	
+				observer.events = {};
 			});
-
 			it("Should remove the callback function from the event", function () {
 				expect(observer.events['newEvent'][0]).toBe(callBack1);
 				expect(observer.events['newEvent'][1]).toBe(callBack2);
 				expect(observer.events['newEvent'][2]).toBe(callBack3);
 				expect(observer.events['newEvent'][3]).not.toBeDefined();
 			});
-
 		});
-				
-		
 	});
-
 	describe("Calling fire() with an event and data", function () {
 		var observer = CreatifyJS.customEvents,
 			data = {name: 'Joe'},
@@ -142,25 +111,19 @@ describe("Custom events", function () {
 				flag = true;
 				expectedData = data;
 			};
-		
 		beforeEach(function () {
 			observer.events = {
 				newEvent: [callBack]
 			};
 			observer.fire('newEvent', data);
 		});
-
 		afterEach(function () {
-			observer.events = {};	
+			observer.events = {};
 		});
-
 		it("Calls callBack() with data", function () {
 			expect(flag).toBe(true);
 			expect(data).toEqual(expectedData);
 			expect(expectedData.name).toBe('Joe');
 		});
-
 	});
-
-
 });
