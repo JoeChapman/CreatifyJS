9b28a5ef76a9d892b154860ea784431d12f4f540
Joseph Chapman <joe@creatifyjs.com>
2012-03-16 09:17:27 +0000

    Complete iterator and tests

--- scripts/CreatifyJS.iterator/CreatifyJS.iterator.js
+++ scripts/CreatifyJS.iterator/CreatifyJS.iterator.js
@@ -2,40 +2,66 @@ var CreatifyJS = CreatifyJS || {};
 
 CreatifyJS.iterator = function (data) {
 
-	var index = 0;
+	var index = 0,
 
-	var count = function(obj) {
-	    var size = 0, key;
-	    for (key in obj) {
-	        size++;
-	    }
-	    return size;
-	};
+		size = function(obj) {
+		    var size = 0, key;
+		    for (key in obj) {
+		        size++;
+		    }
+		    return size;
+		},
 
-	var getItem = function(obj, i) {
-	    var key, arr = [];
-	    for (key in obj) {
-	        arr.push(obj[key]);
-	    }
-	    return arr[i];
-	};
+		getItem = function(obj, i) {
+		    var key, arr = [];
+		    for (key in obj) {
+		        arr.push(obj[key]);
+		    }
+		    return arr[i];
+		},
 
-	var length = data.length ? data.length : count(data);
+		DATATYPE = (function () {
+			return Object.prototype.toString.call(data).match(/\w+/g)[1].toLowerCase();
+		}()),
+
+		length = data.length ? data.length : size(data);
 
 	return {
+
 		next: function () {
 			if (!this.hasNext()) {
                 return null;
             }
 			index += 1;
-			if (data[index] !== undefined) {
-                return data[index];
-            }
-            return getItem(data, index);
+            return data[index] || getItem(data, index);
 		},
+
 		hasNext : function() {
             return index < length-1;
         }, 
+
+        current: function () {
+        	return data[index] || getItem(data, index);
+        },
+
+        getType: function () {
+        	return DATATYPE;
+        },
+
+        last: function () {
+        	var end = length - 1;
+        	return data[end] || getItem(data, end);
+        },
+
+        find: function (pos) {
+        	var position = pos -1;
+        	return data[position] || getItem(data, position);
+        },
+
+        reset: function () {
+        	index = 0;
+        	return this.current();
+        }
 	};
 
 };
--- specs/CreatifyJS.iterator/CreatifyJS.iterator.js
+++ specs/CreatifyJS.iterator/CreatifyJS.iterator.js
@@ -27,8 +27,62 @@ describe("iterator", function() {
 	});
 	describe("Asking if data has next item of hash", function () {
 		var iterator = CreatifyJS.iterator(dataHash);
-		it('Returns a true if another item exists', function () {
+		it('Returns true if another item exists', function () {
 			expect(iterator.hasNext()).toBe(true);
+		});
+	});
+	describe("Asking for the current item of the Array data", function () {
+		var iterator = CreatifyJS.iterator(dataArr);
+		it("Returns the current item", function () {
+			expect(iterator.current()).toBe('joe');
+		});
+	});
+	describe("Asking for the current item of the hash data", function () {
+		var iterator = CreatifyJS.iterator(dataHash);
+		it("Returns the current item", function () {
+			expect(iterator.current()).toBe('joe');
+		});
+	});
+	describe("Asking for the type of data", function () {
+		it("Returns the real data type name (object)", function () {
+			var iterator = CreatifyJS.iterator(dataHash);
+			expect(iterator.getType()).toBe('object');
+		});
+		it("Returns the real data type name (array", function () {
+			var iterator = CreatifyJS.iterator(dataArr);
+			expect(iterator.getType()).toBe('array');
+		});
+	});
+	describe("Asking for the last item of data", function () {
+		it("Returns the last item of data", function () {
+			var iterator = CreatifyJS.iterator(dataHash);
+			expect(iterator.last()).toBe('UK');
+		});
+		it("Returns the last item of data", function () {
+			var iterator = CreatifyJS.iterator(dataArr);
+			expect(iterator.last()).toBe('UK');
+		});
+	});
+	describe("Asking for item by it's position", function () {
+		it("Returns the item at that position", function () {
+			var iterator = CreatifyJS.iterator(dataHash);
+			expect(iterator.find(3)).toBe('developer');
+		});
+		it("Returns the item at that position", function () {
+			var iterator = CreatifyJS.iterator(dataArr);
+			expect(iterator.find(2)).toBe('chapman');
+		});
+	});
+	describe("Resetting", function () {
+		it("Updates the current item to the first", function () {
+			var iterator = CreatifyJS.iterator(dataHash);
+			iterator.next();	
+			expect(iterator.reset()).toBe('joe');
+		});
+		it("Updates the current item to the first", function () {
+			var iterator = CreatifyJS.iterator(dataArr);
+			iterator.last();
+			expect(iterator.reset()).toBe('joe');
 		});
 	});
 });
